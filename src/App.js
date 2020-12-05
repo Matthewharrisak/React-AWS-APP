e710694c3d841523c3d3a67d78e439e1b44dbef1
Matthew Harris <matthewharrisak@gmail.com>
2020-12-05 16:38:59 -0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-    <h1> Emma, this is a test to see if my local files will update with the deployment server, I love you! </h1>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
