21fd735bd4bdee834046eb70805b88ec42c5e566
Matthew Harris <matthewharrisak@gmail.com>
2020-12-05 15:19:57 -0600

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Emma, this is a test to see if my local files will update with the deployment server, I love you! </h1>
     </div>
   );
 }
